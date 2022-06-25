d2845ef63311d7cdb15cb05e7869ec68c213822b
Luxing Li <luxingg.li@gmail.com>
2022-06-25 23:12:52 +1000

    Draw login page

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,201 +1,89 @@
 import React from 'react'
-import { Form, Button, Input } from 'antd'
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import './Login.css'
-import Particles from 'react-tsparticles';
+import "antd/dist/antd.css";
+import "./Login.css";
+import { Button, Checkbox, Form, Input } from "antd";
+import { UserOutlined, LockOutlined, GlobalOutlined } from "@ant-design/icons";
 
 export default function Login() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
 
-    const onFinish = (values) => {
-        console.log(values)
-    }
-    return (
-        <div style={{ background: 'rgb(35, 39, 65)', height: "100%",overflow:'hidden' }}>
-            <Particles height={document.documentElement.clientHeight}params={
-                {
-                    "background": {
-                      "color": {
-                        "value": "rgb(35, 39, 65)"
-                      },
-                      "position": "50% 50%",
-                      "repeat": "no-repeat",
-                      "size": "cover"
-                    },
-                    "fullScreen": {
-                      "enable": true,
-                      "zIndex": 1
-                    },
-                    "interactivity": {
-                      "events": {
-                        "onClick": {
-                          "enable": true,
-                          "mode": "push"
-                        },
-                        "onHover": {
-                          "enable": true,
-                          "mode": "bubble",
-                          "parallax": {
-                            "force": 60
-                          }
-                        }
-                      },
-                      "modes": {
-                        "bubble": {
-                          "distance": 400,
-                          "duration": 2,
-                          "opacity": 1,
-                          "size": 40
-                        },
-                        "grab": {
-                          "distance": 400
-                        }
-                      }
-                    },
-                    "particles": {
-                      "color": {
-                        "value": "#ffffff"
-                      },
-                      "links": {
-                        "color": {
-                          "value": "#fff"
-                        },
-                        "distance": 150,
-                        "opacity": 0.4
-                      },
-                      "move": {
-                        "attract": {
-                          "rotate": {
-                            "x": 600,
-                            "y": 1200
-                          }
-                        },
-                        "enable": true,
-                        "outModes": {
-                          "default": "bounce",
-                          "bottom": "bounce",
-                          "left": "bounce",
-                          "right": "bounce",
-                          "top": "bounce"
-                        },
-                        "speed": 6
-                      },
-                      "number": {
-                        "density": {
-                          "enable": true
-                        },
-                        "value": 170
-                      },
-                      "opacity": {
-                        "animation": {
-                          "speed": 1,
-                          "minimumValue": 0.1
-                        }
-                      },
-                      "shape": {
-                        "options": {
-                          "character": {
-                            "fill": false,
-                            "font": "Verdana",
-                            "style": "",
-                            "value": "*",
-                            "weight": "400"
-                          },
-                          "char": {
-                            "fill": false,
-                            "font": "Verdana",
-                            "style": "",
-                            "value": "*",
-                            "weight": "400"
-                          },
-                          "polygon": {
-                            "nb_sides": 5
-                          },
-                          "star": {
-                            "nb_sides": 5
-                          },
-                          "image": {
-                            "height": 32,
-                            "replace_color": true,
-                            "src": "/logo192.png",
-                            "width": 32
-                          },
-                          "images": {
-                            "height": 32,
-                            "replace_color": true,
-                            "src": "/logo192.png",
-                            "width": 32
-                          }
-                        },
-                        "type": "image"
-                      },
-                      "size": {
-                        "value": 16,
-                        "animation": {
-                          "speed": 40,
-                          "minimumValue": 0.1
-                        }
-                      },
-                      "stroke": {
-                        "color": {
-                          "value": "#000000",
-                          "animation": {
-                            "h": {
-                              "count": 0,
-                              "enable": false,
-                              "offset": 0,
-                              "speed": 1,
-                              "sync": true
-                            },
-                            "s": {
-                              "count": 0,
-                              "enable": false,
-                              "offset": 0,
-                              "speed": 1,
-                              "sync": true
-                            },
-                            "l": {
-                              "count": 0,
-                              "enable": false,
-                              "offset": 0,
-                              "speed": 1,
-                              "sync": true
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-            }/>
-            <div className="formContainer">
-                <div className="logintitle">Global News Release and Management System</div>
-                <Form
-                    name="normal_login"
-                    className="login-form"
-                    onFinish={onFinish}
-                >
-                    <Form.Item
-                        name="username"
-                        rules={[{ required: true, message: 'Please input your Username!' }]}
-                    >
-                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-                    </Form.Item>
-                    <Form.Item
-                        name="password"
-                        rules={[{ required: true, message: 'Please input your Password!' }]}
-                    >
-                        <Input
-                            prefix={<LockOutlined className="site-form-item-icon" />}
-                            type="password"
-                            placeholder="Password"
-                        />
-                    </Form.Item>
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" className="login-form-button">
-                            Login
-                     </Button>
-                    </Form.Item>
-                </Form>
-            </div>
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div style={{ background: 'rgb(35,39,65)', height: '100%' }}>
+      <div className="formContainer">
+        <div className="loginTitle">
+          <GlobalOutlined /> Global news release and management system
         </div>
-    )
+        <Form
+          name="basic"
+          labelCol={{
+            span: 5,
+          }}
+          wrapperCol={{
+            span: 18,
+          }}
+          initialValues={{
+            remember: false
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+
+          <Form.Item
+            label= {<label>Username</label>}
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!"
+              }
+            ]}
+          >
+            <Input   prefix={<UserOutlined />} />
+          </Form.Item>
+
+          <Form.Item
+            label= {<label>Password</label>}
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!"
+              }
+            ]}
+          >
+            <Input.Password   prefix={<LockOutlined />} />
+          </Form.Item>
+
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+              offset: 5,
+              span: 8
+            }}
+          >
+            <Checkbox style={{color:'white'}}>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 5,
+              span: 8
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  )
 }
